20376aeb92f139f83c3c48d1ea6b1aeb79d6d8ad
meena panimalar <115329967+Meenapanimalar@users.noreply.github.com>
2025-05-17 08:50:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-const h = document.querySelector(".hrs")
-const m = document.querySelector(".mins")
-const s = document.querySelector(".sec")
+const buttons = document.querySelectorAll(".btn");
+const images = document.querySelectorAll(".image");
+const search = document.querySelector("#search");
 
-function analog(){
-    const time = new Date();
-    const seconds= (time.getSeconds())/60; 
-    const minute = (seconds+time.getMinutes())/60;
-    const hour = (minute+time.getHours())/12;
 
-    h.style.setProperty('--rotation',hour*360)
-    m.style.setProperty('--rotation',minute*360)
-    s.style.setProperty('--rotation',seconds*360)
+buttons.forEach(button =>{
+    button.addEventListener("click",(e)=>{
+        buttons.forEach(btn => btn.classList.remove("buttonClicked"));
+        e.target.classList.add("buttonClicked");
+
+        const filter=e.target.dataset.filter.toLowerCase();
+
+        images.forEach(image =>{
+            const data = image.dataset.items;
+            if(data===filter|| filter=="all"){
+                image.style.display="block";
+            }else{
+                image.style.display="none";
+            }
+        })
+    })
+})
+search.addEventListener('keyup',(e)=>{
+   var searchtext = e.target.value.toLowerCase().trim();
+
+
+// e is the event object automatically passed to the event handler function by the browser when the keyup event is fired.
+// It contains information about the event, such as which key was pressed, which element triggered the event, and more.
+
+// e.target: This property references the element that triggered the event. In this case, it is the search input field.
+// e.target.value: This property gets the current value of the input field at the time the event was triggered.
+
+images.forEach((image)=>{
+    const data = image.dataset.items;
+    if(data===searchtext ||searchtext===""){
+        image.style.display="block";
+    }
+    else{
+        image.style.display="none";
+    }
     
-
-
-}
-setInterval(analog,1000)
+})})
